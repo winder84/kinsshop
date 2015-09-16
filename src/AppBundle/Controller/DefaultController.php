01e7f80eb34554b172707e217b950ef8312e2c8e
winder84 <winder84@mail.ru>
2015-09-16 21:42:07 +0300

    view

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,13 +14,14 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+//        return $this->render('AppBundle:Default:index.html.twig', array(
+//            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+//        ));
+        return $this->render('AppBundle:Default:index.html.twig', array());
     }
 
     /**
-     * @Route("/site/{alias}", name="homepage")
+     * @Route("/site/{alias}")
      */
     public function siteAction($alias)
     {
