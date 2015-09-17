79ef6bdbd1fdd8f209a7118c7a538792dd2f9b8a
winder84 <winder84@mail.ru>
2015-09-18 00:07:49 +0300

    v.1.0

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,11 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    private $exCategoriesIds;
+    private $categories;
+    private $sites;
+
     /**
-     * @Route("/{page}", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request, $page = 0)
+    public function indexAction(Request $request)
     {
+        $page = 0;
         $em = $this->getDoctrine()->getManager();
         $products = $em
             ->getRepository('AppBundle:Product')
@@ -29,7 +34,7 @@ class DefaultController extends Controller
         $engine = $this->container->get('templating');
         $paginator =  $engine->render('AppBundle:Default:paginator.html.twig', array(
                 'paginator' => $paginatorData,
-                'path' => '/',
+                'path' => '/page/',
             )
         );
         $products = $em
@@ -51,12 +56,6 @@ class DefaultController extends Controller
             ->setMaxResults(12);
         $query = $qb->getQuery();
         $vendors = $query->getResult();
-        $sites = $em
-            ->getRepository('AppBundle:Site')
-            ->findAll();
-        $categories = $em
-            ->getRepository('AppBundle:Category')
-            ->findAll();
         foreach ($products as $product) {
             $resultProducts[] = array(
                 'name' => $product->getName(),
@@ -70,11 +69,77 @@ class DefaultController extends Controller
 //            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
 //        ));
 
+        $this->getMenuItems();
         return $this->render('AppBundle:Default:index.html.twig', array(
             'products' => $resultProducts,
-            'sites' => $sites,
+            'sites' => $this->sites,
             'vendors' => $vendors,
-            'categories' => $categories,
+            'categories' => $this->categories,
+            'paginator' => $paginator,
+        ));
+    }
+
+    /**
+     * @Route("/page/{page}")
+     */
+    public function indexPageAction(Request $request, $page = 0)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $products = $em
+            ->getRepository('AppBundle:Product')
+            ->findBy(
+                array(),
+                array(),
+                160,
+                0
+            );
+        $productsCount = count($products);
+        $paginatorPagesCount = floor($productsCount / 16);
+        $paginatorData = new \AppBundle\Helpers\Paginator($paginatorPagesCount, $page, 1, 5);
+        $engine = $this->container->get('templating');
+        $paginator =  $engine->render('AppBundle:Default:paginator.html.twig', array(
+                'paginator' => $paginatorData,
+                'path' => '/page/',
+            )
+        );
+        $products = $em
+            ->getRepository('AppBundle:Product')
+            ->findBy(
+                array(),
+                array(),
+                16,
+                16 * $page
+            );
+        $qb = $em->createQueryBuilder();
+        $qb->select('Vendor, count(Vendor) as cnt')
+            ->from('AppBundle:Product', 'Product')
+            ->leftJoin('AppBundle:Vendor', 'Vendor')
+            ->where('Vendor = Product.vendor')
+            ->groupBy('Vendor')
+            ->orderBy('cnt', 'DESC')
+            ->setFirstResult(0)
+            ->setMaxResults(12);
+        $query = $qb->getQuery();
+        $vendors = $query->getResult();
+        foreach ($products as $product) {
+            $resultProducts[] = array(
+                'name' => $product->getName(),
+                'model' => $product->getModel(),
+                'pictures' => $product->getPictures(),
+                'id' => $product->getId(),
+            );
+        }
+        // replace this example code with whatever you need
+//        return $this->render('AppBundle:Default:index.html.twig', array(
+//            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+//        ));
+
+        $this->getMenuItems();
+        return $this->render('AppBundle:Default:index.html.twig', array(
+            'products' => $resultProducts,
+            'sites' => $this->sites,
+            'vendors' => $vendors,
+            'categories' => $this->categories,
             'paginator' => $paginator,
         ));
     }
@@ -103,16 +168,11 @@ class DefaultController extends Controller
         $query = $qb->getQuery();
         $vendors = $query->getResult();
 
-        $categories = $em
-            ->getRepository('AppBundle:Category')
-            ->findAll();
-        $sites = $em
-            ->getRepository('AppBundle:Site')
-            ->findAll();
+        $this->getMenuItems();
         return $this->render('AppBundle:Default:site.description.html.twig', array(
                 'site' => $site,
-                'sites' => $sites,
-                'categories' => $categories,
+                'sites' => $this->sites,
+                'categories' => $this->categories,
                 'vendors' => $vendors
             )
         );
@@ -142,13 +202,10 @@ class DefaultController extends Controller
         $query = $qb->getQuery();
         $vendors = $query->getResult();
 
-        $sites = $em
-            ->getRepository('AppBundle:Site')
-            ->findAll();
         $qb = $em->createQueryBuilder();
         $qb->select('Product')
             ->from('AppBundle:Product', 'Product')
-            ->andWhere('Product.site = :site')
+            ->where('Product.site = :site')
             ->setParameter('site', $site);
         $query = $qb->getQuery()
             ->setFirstResult(28 * $page)
@@ -165,13 +222,11 @@ class DefaultController extends Controller
             )
         );
 
-        $categories = $em
-            ->getRepository('AppBundle:Category')
-            ->findAll();
+        $this->getMenuItems();
         return $this->render('AppBundle:Default:site.html.twig', array(
                 'site' => $site,
-                'sites' => $sites,
-                'categories' => $categories,
+                'sites' => $this->sites,
+                'categories' => $this->categories,
                 'products' => $products,
                 'paginator' => $paginator,
                 'vendors' => $vendors
@@ -188,18 +243,10 @@ class DefaultController extends Controller
         $vendor = $em
             ->getRepository('AppBundle:Vendor')
             ->findOneBy(array('alias' => $alias));
-
-        $categories = $em
-            ->getRepository('AppBundle:Category')
-            ->findAll();
-
-        $sites = $em
-            ->getRepository('AppBundle:Site')
-            ->findAll();
         $qb = $em->createQueryBuilder();
         $qb->select('Product')
             ->from('AppBundle:Product', 'Product')
-            ->andWhere('Product.vendor = :vendor')
+            ->where('Product.vendor = :vendor')
             ->setParameter('vendor', $vendor);
         $query = $qb->getQuery()
             ->setFirstResult(28 * $page)
@@ -215,13 +262,159 @@ class DefaultController extends Controller
                 'path' => '/vendor/' . $alias . '/',
             )
         );
+        $this->getMenuItems();
         return $this->render('AppBundle:Default:vendor.html.twig', array(
                 'products' => $products,
                 'paginator' => $paginator,
-                'sites' => $sites,
+                'sites' => $this->sites,
                 'vendor' => $vendor,
-                'categories' => $categories
+                'categories' => $this->categories
             )
         );
+    }
+
+    /**
+     * @Route("/category/{alias}/{page}")
+     */
+    public function categoryAction($alias, $page = 0)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $category = $em
+            ->getRepository('AppBundle:Category')
+            ->findOneBy(array('alias' => $alias));
+
+        $externalCategories = $category->getExternalCategories();
+        foreach ($externalCategories as $externalCategory ) {
+            $this->exCategoriesIds[] = $externalCategory->getExternalId();
+        }
+        $this->getCategoriesIdsRecursive();
+        $qb = $em->createQueryBuilder();
+        $qb->select('Product')
+            ->from('AppBundle:Product', 'Product')
+            ->where('Product.category IN (:exCategoriesIds)')
+            ->setParameter('exCategoriesIds', $this->exCategoriesIds);
+        $query = $qb->getQuery()
+            ->setFirstResult(28 * $page)
+            ->setMaxResults(28);
+        $products = new Paginator($query, $fetchJoinCollection = true);
+
+        $productsCount = count($products);
+        $paginatorPagesCount = floor($productsCount / 28);
+        $paginatorData = new \AppBundle\Helpers\Paginator($paginatorPagesCount, $page, 1, 5);
+        $engine = $this->container->get('templating');
+        $paginator =  $engine->render('AppBundle:Default:paginator.html.twig', array(
+                'paginator' => $paginatorData,
+                'path' => '/category/' . $alias . '/',
+            )
+        );
+        $qb->select('ExCategory')
+            ->from('AppBundle:ExternalCategory', 'ExCategory')
+            ->where('ExCategory.id IN (:exCategoriesIds)')
+            ->setParameter('exCategoriesIds', $this->exCategoriesIds);
+        $query = $qb->getQuery()
+            ->setMaxResults(18);
+        $exCategories = $query->getResult();
+
+        $this->getMenuItems();
+        return $this->render('AppBundle:Default:category.html.twig', array(
+                'products' => $products,
+                'paginator' => $paginator,
+                'category' => $category,
+                'exCategories' => $exCategories,
+                'sites' => $this->sites,
+                'categories' => $this->categories
+            )
+        );
+    }
+
+    /**
+     * @Route("/exCategory/{id}/{page}")
+     */
+    public function exCategoryAction($id, $page = 0)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $category = $em
+            ->getRepository('AppBundle:ExternalCategory')
+            ->findOneBy(array('id' => $id));
+        $qb = $em->createQueryBuilder();
+        $qb->select('Product')
+            ->from('AppBundle:Product', 'Product')
+            ->where('Product.category = :category')
+            ->setParameter('category', $category);
+        $query = $qb->getQuery()
+            ->setFirstResult(28 * $page)
+            ->setMaxResults(28);
+        $products = new Paginator($query, $fetchJoinCollection = true);
+
+        $productsCount = count($products);
+        $paginatorPagesCount = floor($productsCount / 28);
+        $paginatorData = new \AppBundle\Helpers\Paginator($paginatorPagesCount, $page, 1, 5);
+        $engine = $this->container->get('templating');
+        $paginator =  $engine->render('AppBundle:Default:paginator.html.twig', array(
+                'paginator' => $paginatorData,
+                'path' => '/exCategory/' . $id . '/',
+            )
+        );
+
+        $this->getMenuItems();
+        return $this->render('AppBundle:Default:exCategory.html.twig', array(
+                'products' => $products,
+                'paginator' => $paginator,
+                'category' => $category,
+                'sites' => $this->sites,
+                'categories' => $this->categories
+            )
+        );
+    }
+
+    /**
+     * @Route("/product/{id}")
+     */
+    public function productAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $product = $em
+            ->getRepository('AppBundle:Product')
+            ->findOneBy(array('id' => $id));
+
+        $this->getMenuItems();
+        return $this->render('AppBundle:Default:product.description.html.twig', array(
+                'sites' => $this->sites,
+                'product' => $product,
+                'categories' => $this->categories
+            )
+        );
+    }
+
+    private function getCategoriesIdsRecursive()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        $qb->select('ExCategory.id')
+            ->from('AppBundle:ExternalCategory', 'ExCategory')
+            ->where('ExCategory.parentId IN (:exCategoriesIds)')
+            ->andWhere('ExCategory.id NOT IN (:exCategoriesIds)')
+            ->setParameter('exCategoriesIds', $this->exCategoriesIds);
+        $query = $qb->getQuery();
+        $resultExCategories = $query->getResult();
+        if (empty($resultExCategories)) {
+            return $this->exCategoriesIds;
+        } else {
+            foreach ($resultExCategories as $externalCategory ) {
+                $this->exCategoriesIds[] = $externalCategory['id'];
+            }
+            $this->getCategoriesIdsRecursive();
+        }
+    }
+
+    private function getMenuItems()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $this->categories = $em
+            ->getRepository('AppBundle:Category')
+            ->findAll();
+        $this->sites = $em
+            ->getRepository('AppBundle:Site')
+            ->findAll();
     }
 }
