c9f281a40076cfb03c5871827293fc817323bbc9
rustam <rustam@nodasoft.com>
2015-10-26 08:59:21 +0300

    sitemap fix

--- src/AppBundle/EventListener/SitemapListener.php
+++ src/AppBundle/EventListener/SitemapListener.php
@@ -27,26 +27,37 @@ class SitemapListener implements SitemapListenerInterface
             $urls[] = $this->router->generate('homepage', array(), true);
         }
 
-        foreach ($this->em->getRepository('AppBundle:Site')->findAll() as $site) {
+        $sites = $this->em->getRepository('AppBundle:Site')->findAll();
+        foreach ($sites as $site) {
             $urls[] = $this->router->generate('shop_description_route', array('alias' => $site->getAlias()), true);
         }
-        foreach ($this->em->getRepository('AppBundle:Vendor')->findAll() as $vendor) {
+        $sites = null;
+
+        $vendors = $this->em->getRepository('AppBundle:Vendor')->findAll();
+        foreach ($vendors as $vendor) {
             $urls[] = $this->router->generate('vendor_route', array('alias' => $vendor->getAlias()), true);
         }
+        $vendors= null;
 
-        foreach ($this->em->getRepository('AppBundle:ExternalCategory')->findAll() as $exCategory) {
+        $exCategories = $this->em->getRepository('AppBundle:ExternalCategory')->findAll();
+        foreach ($exCategories as $exCategory) {
             $urls[] = $this->router->generate('ex_category_route', array('id' => $exCategory->getId()), true);
         }
+        $exCategories = null;
 
-        foreach ($this->em->getRepository('AppBundle:Category')->findAll() as $category) {
+        $categories = $this->em->getRepository('AppBundle:Category')->findAll();
+        foreach ($categories as $category) {
             $urls[] = $this->router->generate('category_route', array('alias' => $category->getAlias()), true);
         }
+        $categories = null;
 
-        foreach ($this->em->getRepository('AppBundle:Product')->findAll() as $product) {
+        $products = $this->em->getRepository('AppBundle:Product')->findAll();
+        foreach ($products as $product) {
             if (!$product->getIsDelete()) {
                 $urls[] = $this->router->generate('product_route', array('id' => $product->getId()), true);
             }
         }
+        $products = null;
 
         foreach ($urls as $url) {
             $event->getGenerator()->addUrl(
